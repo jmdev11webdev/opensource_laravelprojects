c2b81b397f6e4a03233a7063a51c12a1f3bebbb3
jmdevstack <jmdev.11.fsd@gmail.com>
2026-04-06 15:02:25 +0800

    Delete simplefree_linkinbio_builder directory

--- simplefree_linkinbio_builder/resources/views/links/index.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layouts::app :title="__('Links')">
-    <div class="flex h-full w-full flex-1 flex-col gap-6 p-6">
-        <div class="max-w-3xl w-full mx-auto">
-            <livewire:list />
-        </div>
-    </div>
-</x-layouts::app>
